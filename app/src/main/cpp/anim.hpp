94739303aef155a8fb022754f98ab919a3c484e2
Kairat Doshekenov <kayrat@bk.ru>
2018-05-07 09:18:40 +0600

    init

---
+++ app/src/main/cpp/anim.hpp
@@ -0,0 +1,11 @@
+#ifndef endlesstunnel_anim_hpp
+#define endlesstunnel_anim_hpp
+
+#include "shape_renderer.hpp"
+
+/* Renders a the background animation seen on the main screen and menus (the parallax
+ * rectangles scrolling by). */
+void RenderBackgroundAnimation(ShapeRenderer *r);
+
+#endif
+
---
+++ app/src/main/java/zhet/tunnel/activity/Main.java
@@ -0,0 +1,31 @@
+package zhet.tunnel.activity;
+
+import android.app.Activity;
+import android.app.NativeActivity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import zhet.tunnel.R;
+
+
+public class Main extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new Thread() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(2000);
+                    runOnUiThread(() -> {
+                        startActivity(new Intent(Main.this, NativeActivity.class));
+                        finish();
+                    });
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+    }
+}
